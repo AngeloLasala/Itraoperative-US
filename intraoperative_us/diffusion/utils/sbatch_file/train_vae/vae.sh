59acdb0de90b9f9f3d59eb2ad816b8c1fa4373dc
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-02-17 10:02:15 +0000

    initial import guideline for SD in diffuser

---
+++ intraoperative_us/diffusion/utils/sbatch_file/train_vae/vae.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=9:50:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=vae_all.err       # standard error file
+#SBATCH --output=vae_all.out      # standard output file
+#SBATCH --account=IscrC_Med-LMGM     # account name
+
+python -m intraoperative_is.diffusion.tools.train_vae --data eco_image_cond\\
+                                                      --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/ius_diffusion'
+ 
