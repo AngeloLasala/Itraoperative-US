cd545b9fd12ee6c0801a504ab9a21937cb709746
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-02-17 10:05:31 +0000

    inmport sbatch file for train vae

--- intraoperative_us/diffusion/utils/sbatch_file/train_vae/vae.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/train_vae/vae.sh
@@ -9,6 +9,5 @@
 #SBATCH --output=vae_all.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
-python -m intraoperative_us.diffusion.tools.train_vae --data eco_image_cond\\
-                                                      --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/ius_diffusion'
+python -m intraoperative_us.diffusion.tools.train_vae --data eco_image_cond --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/ius_diffusion'
  
