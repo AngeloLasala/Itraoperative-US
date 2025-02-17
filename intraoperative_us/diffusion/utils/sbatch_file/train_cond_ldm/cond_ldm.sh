69e5bbec988c925962b08ccff5bfef8067baec22
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-02-17 17:11:33 +0000

    initial import sbatch file for tratin cond ldm

--- intraoperative_us/diffusion/utils/sbatch_file/train_cond_ldm/cond_ldm.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/train_cond_ldm/cond_ldm.sh
@@ -5,8 +5,8 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=18:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=simple_heat_cond_ldm.err       # standard error file
-#SBATCH --output=simple_heat_cond_ldm.out      # standard output file
+#SBATCH --error=cond_ldm.err       # standard error file
+#SBATCH --output=cond_ldm.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
 python -m intraoperative_us.diffusion.tools.train_cond_ldm --data conf\
