e230d0415518757897d38a9afb7eff5e77560e27
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-02-18 12:39:36 +0000

    upload dataset.sh

---
+++ intraoperative_us/diffusion/dataset/dataset.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=9:50:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=vae_all.err       # standard error file
+#SBATCH --output=vae_all.out      # standard output file
+#SBATCH --account=IscrC_Med-LMGM     # account name
+
+python -m intraoperative_us.diffusion.dataset.dataset
