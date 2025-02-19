2ea09112cc3ac255de93d5d654bf6fa72069ab92
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-02-19 09:03:29 +0000

    upload longer conditional diffusion training

--- intraoperative_us/diffusion/utils/adapt_new_model.py
+++ intraoperative_us/diffusion/utils/adapt_new_model.py
@@ -9,8 +9,29 @@ a pretrained UNet2DConditionModel.
 """
 from diffusers import StableDiffusionPipeline
 from intraoperative_us.diffusion.utils.utils import get_numer_parameter
+from diffusers import UNet2DConditionModel
 
+## Load all the models in Stable Diffusion
 pipeline = StableDiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
 unet = pipeline.unet
+get_numer_parameter(unet)
+# for key in unet.config:
+#     print(key, unet.config[key])
+
+
+## adapt the model to a new unput shape
+# Initialize a UNet2DConditionModel with the pretrained text-to-image model weights,
+# and change in_channels to 9. Changing the number of in_channels means
+# you need to set ignore_mismatched_sizes=True and low_cpu_mem_usage=False
+# to avoid a size mismatch error because the shape is different now.  
+model_id = "runwayml/stable-diffusion-v1-5"
+unet = UNet2DConditionModel.from_pretrained(
+    model_id, subfolder="unet",
+    in_channels=1,
+    sample_size=32,
+    low_cpu_mem_usage=False,
+    ignore_mismatched_sizes=True
+)
+print(unet.config.in_channels, unet.config.sample_size)
 
 get_numer_parameter(unet)
--- intraoperative_us/diffusion/utils/sbatch_file/sample_cond_ldm/samle_cond_ldm.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/sample_cond_ldm/samle_cond_ldm.sh
@@ -10,12 +10,12 @@
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
 for trial in trial_3; do
-    for w in 0.0 0.2 0.4 0.6; do
-        for epoch in 20 40 60 80 100 120 140 150; do
+    for w in 0.5 1.0 2.0; do
+        for epoch in 150 300 600 900 1200 1500; do
                 python -m intraoperative_us.diffusion.tools.sample_cond_ldm\
                         --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/ius_diffusion'\
                         --trial $trial\
-                        --experiment cond_ldm_1\
+                        --experiment cond_ldm_2\
                         --epoch $epoch\
                         --guide_w $w\
 
