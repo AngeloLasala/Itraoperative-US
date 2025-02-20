766f2f3345fac62e4dcaee3f76a77935d360db77
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-02-20 12:52:37 +0000

    upload tarin ldm

--- intraoperative_us/diffusion/utils/sbatch_file/train_ldm/ldm.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/train_ldm/ldm.sh
@@ -5,8 +5,8 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=24:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=cond_ldm.err       # standard error file
-#SBATCH --output=cond_ldm.out      # standard output file
+#SBATCH --error=ldm.err       # standard error file
+#SBATCH --output=ldm.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
 python -m intraoperative_us.diffusion.tools.train_ldm --conf conf_mask\
