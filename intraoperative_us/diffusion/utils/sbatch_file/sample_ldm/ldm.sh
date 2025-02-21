df2c77cf48d176dcbb508a41a1be7b48906894a6
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-02-21 12:02:20 +0000

    uplaod sample_ldm Only for mask

--- intraoperative_us/diffusion/utils/sbatch_file/sample_ldm/ldm.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/sample_ldm/ldm.sh
@@ -5,8 +5,8 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=24:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=sample_ldm.err       # standard error file
-#SBATCH --output=sample_ldm.out      # standard output file
+#SBATCH --error=ldm.err       # standard error file
+#SBATCH --output=ldm.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
 for trial in trial_1; do
@@ -14,7 +14,7 @@ for trial in trial_1; do
             python -m intraoperative_us.diffusion.tools.sample_ldm\
                     --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/ius_diffusion'\
                     --trial $trial\
-                    --experiment cond_ldm_1\
+                    --experiment ldm_1\
                     --type_image mask\
                     --epoch $epoch\
 
