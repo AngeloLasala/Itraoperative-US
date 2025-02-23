f3862adf80862ab4e146d782010767c2bbb228b8
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-02-23 17:29:13 +0000

    add conf file for fine tuning the hugginface model

--- intraoperative_us/diffusion/utils/sbatch_file/vae/vae.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/vae/vae.sh
@@ -5,11 +5,11 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=9:50:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=vae_hug.err       # standard error file
-#SBATCH --output=vae_hug.out      # standard output file
+#SBATCH --error=vae_fine_hug.err       # standard error file
+#SBATCH --output=vae_fine_hug.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
-python -m intraoperative_us.diffusion.tools.train_vae --conf conf\
+python -m intraoperative_us.diffusion.tools.train_vae --conf conf_fine_tuning\
                                                       --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/ius_diffusion'\
                                                       --log 'info'
  
