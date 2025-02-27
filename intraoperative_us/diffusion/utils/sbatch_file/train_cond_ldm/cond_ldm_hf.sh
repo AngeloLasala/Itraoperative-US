e3790f56943f345a2a305cbe5440cc5503e6928c
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-02-27 18:20:22 +0000

    initial import of cond_hf

---
+++ intraoperative_us/diffusion/utils/sbatch_file/train_cond_ldm/cond_ldm_hf.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=18:00:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=cond_ldm_hf.err       # standard error file
+#SBATCH --output=cond_ldm_hf.out      # standard output file
+#SBATCH --account=IscrC_Med-LMGM     # account name
+
+python -m intraoperative_us.diffusion.tools.train_cond_ldm_hugginface --conf conf\
+          --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/ius_diffusion'\
+          --trial trial_12\
+          --log warning
