c7fca173a0440b5ec8a034c1be8da4f11b8fa938
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-03-11 12:11:22 +0000

    updating: traind latent diffusion model for tumor mask generation with hugginface pipeline

--- intraoperative_us/diffusion/utils/sbatch_file/train_ldm/ldm.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/train_ldm/ldm.sh
@@ -5,12 +5,13 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=24:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=ldm.err       # standard error file
-#SBATCH --output=ldm.out      # standard output file
+#SBATCH --error=ldm_hf.err       # standard error file
+#SBATCH --output=ldm_hf.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
 python -m intraoperative_us.diffusion.tools.train_ldm --conf conf_mask\
           --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/ius_diffusion'\
-          --trial trial_2\
+          --trial small_vae\
           --type_image mask\
+          --experminet_name uncond_ldm\
           --log info
