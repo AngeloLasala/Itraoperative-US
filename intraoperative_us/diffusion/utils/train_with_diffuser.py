cf3a86fe1954de135bc44f588d83346c58ffe48a
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-03-12 15:52:22 +0000

    updating all

--- intraoperative_us/diffusion/utils/train_with_diffuser.py
+++ intraoperative_us/diffusion/utils/train_with_diffuser.py
@@ -18,13 +18,12 @@ from tqdm.auto import tqdm
 from pathlib import Path
 import os
 
-
 import yaml
 import torch
 import os
 import json
-from torch.utils.data.dataloader import DataLoader
-from intraoperative_us.diffusion.dataset.dataset import IntraoperativeUS
+
+import matplotlib.pyplot as plt
 
 
 @dataclass
@@ -39,7 +38,7 @@ class TrainingConfig:
     save_image_epochs: int = 10
     save_model_epochs: int = 30
     mixed_precision: str = 'fp16'             # `no` for float32, `fp16` for automatic mixed precision
-    output_dir: str = 'ius'  # the model name locally and on the HF Hub
+    output_dir: str = 'betterfly'  # the model name locally and on the HF Hub
 
     push_to_hub: bool = False                  # whether to upload the saved model to the HF Hub
     hub_private_repo: bool = False  
@@ -74,42 +73,14 @@ config = TrainingConfig()
 ## load dataset - ius
 config.dataset_name = "huggan/smithsonian_butterflies_subset"
 dataset = load_dataset(config.dataset_name, split="train")
+print(dataset)
 
-fig, axs = plt.subplots(1, 4, figsize=(16, 4))
+fig, axs = plt.subplots(1, 4, figsize=(16, 4), num='Example of images')
 for i, image in enumerate(dataset[:4]["image"]):
     axs[i].imshow(image)
     axs[i].set_axis_off()
-fig.show()
+# plt.show()
 
-# configuration = "/home/angelo/Documenti/Itraoperative-US/intraoperative_us/diffusion/conf/conf.yaml"
-# with open(configuration, 'r') as file:
-#         try:
-#             configuration = yaml.safe_load(file)
-#         except yaml.YAMLError as exc:
-#             logging.warning(exc)
-# # print(config)
-# dataset_config = configuration['dataset_params']
-# autoencoder_config = configuration['autoencoder_params']
-# train_config = configuration['train_params']
-
-# data = IntraoperativeUS(size= [config.image_size, config.image_size],
-#                             dataset_path= dataset_config['dataset_path'],
-#                             im_channels= dataset_config['im_channels'], 
-#                             splitting_json=dataset_config['splitting_json'],
-#                             split='train',
-#                             splitting_seed=dataset_config['splitting_seed'],
-#                             train_percentage=dataset_config['train_percentage'],
-#                             val_percentage=dataset_config['val_percentage'],
-#                             test_percentage=dataset_config['test_percentage'],
-#                             condition_config=configuration['autoencoder_params']['condition_config'],
-#                             data_augmentation=False,
-#                             rgb=True)
-print(dataset)
-# print(data)
-# fig, axs = plt.subplots(1, 4, figsize=(16, 4))
-# for i, image in enumerate(dataset[:4]["image"]):
-#     axs[i].imshow(image)
-#     axs[i].set_axis_off()
 
 ## prepocess the dataset
 preprocess = transforms.Compose(
@@ -126,20 +97,16 @@ def transform(examples):
     return {"images": images}
 
 dataset.set_transform(transform)
-fig, axs = plt.subplots(1, 4, figsize=(16, 4))
-# for i, image in enumerate(dataset[:4]["images"]):
-#     axs[i].imshow(image.permute(1, 2, 0).numpy() / 2 + 0.5)
-#     axs[i].set_axis_off()
-  
+
+fig, axs = plt.subplots(1, 4, figsize=(16, 4), num='Example of preprocess images')
+for i, image in enumerate(dataset[:4]["images"]):
+    axs[i].imshow(image.permute(1, 2, 0).numpy() / 2 + 0.5)
+    axs[i].set_axis_off()
+# plt.show()
 
 
-# ## data loader
-# train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=config.train_batch_size, shuffle=True)
+# data loader
 train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=config.train_batch_size, shuffle=True)
-
-# for batch in train_dataloader:
-#     print(batch["images"].shape)
-#     break   
 
 
 ## model
@@ -170,95 +137,91 @@ model = UNet2DModel(
 ##  SCHEDULER with example
 noise_scheduler = PNDMScheduler.from_pretrained("sd-legacy/stable-diffusion-v1-5", subfolder="scheduler")
 
-sample_image = dataset[0]["images"].unsqueeze(0)
-print(sample_image.shape)
-image = torch.zeros(8, 3, 56, 56)
-## create a mask for a square of 1
-mask = torch.zeros(1, 1, 128, 128)
-mask[:, :, 20:30, 20:30] = 1
+sample_image = dataset[60]["images"].unsqueeze(0)
+noise_scheduler = DDPMScheduler(num_train_timesteps=1000)
 noise = torch.randn(sample_image.shape)
-timesteps = torch.LongTensor([999])
-noisy_image = noise_scheduler.add_noise(mask, noise, timesteps)
-plt.figure()
-plt.imshow(noisy_image[0].permute(1, 2, 0).numpy())
-plt.show()
+timesteps = torch.LongTensor([50])
+noisy_image = noise_scheduler.add_noise(sample_image, noise, timesteps)
+
+fig, axs = plt.subplots(1, 4, figsize=(16, 4), num='Adding noise with scheduler')
+for i, timesteps in enumerate([250, 500, 750, 999]):
+    timesteps = torch.LongTensor([timesteps])
+    noisy_image = noise_scheduler.add_noise(sample_image, noise, timesteps)
+    axs[i].imshow(noisy_image[0].permute(1, 2, 0).numpy() / 2 + 0.5)
+    axs[i].axis('off')
+plt.show() 
 
 
 ## IINITIALIZATION OF THE TRAINING
+                                     ## MSE los for the predicted noise
+optimizer = torch.optim.AdamW(model.parameters(), lr=config.learning_rate)    ## AdamW optimizer    
+lr_scheduler = get_cosine_schedule_with_warmup(
+    optimizer=optimizer,
+    num_warmup_steps=config.lr_warmup_steps,
+    num_training_steps=(len(train_dataloader) * config.num_epochs),
+)
 
-# noise_pred = model(noisy_image, timesteps).sample
-
-
-# loss = F.mse_loss(noise_pred, noise)                                          ## MSE los for the predicted noise
-# optimizer = torch.optim.AdamW(model.parameters(), lr=config.learning_rate)    ## AdamW optimizer    
-# lr_scheduler = get_cosine_schedule_with_warmup(
-#     optimizer=optimizer,
-#     num_warmup_steps=config.lr_warmup_steps,
-#     num_training_steps=(len(train_dataloader) * config.num_epochs),
-# )
-
-# def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler):
-#     # Initialize accelerator and tensorboard logging
-#     accelerator = Accelerator(
-#         mixed_precision=config.mixed_precision,
-#         gradient_accumulation_steps=config.gradient_accumulation_steps, 
-#     )
-#     if accelerator.is_main_process:
-#         print("sono dentrooooooo")
-#         os.makedirs(config.output_dir, exist_ok=True)
-#         accelerator.init_trackers("train_example")
+def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler):
+    # Initialize accelerator and tensorboard logging
+    accelerator = Accelerator(
+        mixed_precision=config.mixed_precision,
+        gradient_accumulation_steps=config.gradient_accumulation_steps, 
+    )
+    if accelerator.is_main_process:
+        os.makedirs(config.output_dir, exist_ok=True)
+        accelerator.init_trackers("train_example")
     
-#     # Prepare everything
-#     # There is no specific order to remember, you just need to unpack the 
-#     # objects in the same order you gave them to the prepare method.
-#     model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
-#         model, optimizer, train_dataloader, lr_scheduler
-#     )
+    # Prepare everything
+    # There is no specific order to remember, you just need to unpack the 
+    # objects in the same order you gave them to the prepare method.
+    model, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
+        model, optimizer, train_dataloader, lr_scheduler
+    )
     
-#     global_step = 0
+    global_step = 0
 
-#     # Now you train the model
-#     for epoch in range(config.num_epochs):
-#         progress_bar = tqdm(total=len(train_dataloader), disable=not accelerator.is_local_main_process)
-#         progress_bar.set_description(f"Epoch {epoch}")
+    # Now you train the model
+    for epoch in range(config.num_epochs):
+        progress_bar = tqdm(total=len(train_dataloader), disable=not accelerator.is_local_main_process)
+        progress_bar.set_description(f"Epoch {epoch}")
 
-#         for step, batch in enumerate(train_dataloader):
-#             clean_images = batch
-#             # Sample noise to add to the images
-#             noise = torch.randn(clean_images.shape).to(clean_images.device)
-#             bs = clean_images.shape[0]
+        for step, batch in enumerate(train_dataloader):
+            clean_images = batch["images"]
+            # Sample noise to add to the images
+            noise = torch.randn(clean_images.shape).to(clean_images.device)
+            bs = clean_images.shape[0]
 
-#             # Sample a random timestep for each image
-#             timesteps = torch.randint(0, noise_scheduler.num_train_timesteps, (bs,), device=clean_images.device).long()
+            # Sample a random timestep for each image
+            timesteps = torch.randint(0, noise_scheduler.num_train_timesteps, (bs,), device=clean_images.device).long()
 
-#             # Add noise to the clean images according to the noise magnitude at each timestep
-#             # (this is the forward diffusion process)
-#             noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
+            # Add noise to the clean images according to the noise magnitude at each timestep
+            # (this is the forward diffusion process)
+            noisy_images = noise_scheduler.add_noise(clean_images, noise, timesteps)
             
-#             with accelerator.accumulate(model):
-#                 # Predict the noise residual
-#                 noise_pred = model(noisy_images, timesteps, return_dict=False)[0]
-#                 loss = F.mse_loss(noise_pred, noise)
-#                 accelerator.backward(loss)
+            with accelerator.accumulate(model):
+                # Predict the noise residual
+                noise_pred = model(noisy_images, timesteps, return_dict=False)[0]
+                loss = F.mse_loss(noise_pred, noise)
+                accelerator.backward(loss)
 
-#                 accelerator.clip_grad_norm_(model.parameters(), 1.0)
-#                 optimizer.step()
-#                 lr_scheduler.step()
-#                 optimizer.zero_grad()
+                accelerator.clip_grad_norm_(model.parameters(), 1.0)
+                optimizer.step()
+                lr_scheduler.step()
+                optimizer.zero_grad()
             
-#             progress_bar.update(1)
-#             logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0], "step": global_step}
-#             progress_bar.set_postfix(**logs)
-#             accelerator.log(logs, step=global_step)
-#             global_step += 1
+            progress_bar.update(1)
+            logs = {"loss": loss.detach().item(), "lr": lr_scheduler.get_last_lr()[0], "step": global_step}
+            progress_bar.set_postfix(**logs)
+            accelerator.log(logs, step=global_step)
+            global_step += 1
 
-#         # After each epoch you optionally sample some demo images with evaluate() and save the model
-#         if accelerator.is_main_process and epoch % config.save_model_epochs == 0:
-#             pipeline = DDPMPipeline(unet=accelerator.unwrap_model(model), scheduler=noise_scheduler)
-#             evaluate(config, epoch, pipeline)
+        # After each epoch you optionally sample some demo images with evaluate() and save the model
+        if accelerator.is_main_process and epoch % config.save_model_epochs == 0:
+            pipeline = DDPMPipeline(unet=accelerator.unwrap_model(model), scheduler=noise_scheduler)
+            evaluate(config, epoch, pipeline)
 
-# args = (config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler)
-# notebook_launcher(train_loop, args, num_processes=1)
+args = (config, model, noise_scheduler, optimizer, train_dataloader, lr_scheduler)
+notebook_launcher(train_loop, args, num_processes=1)
 
-# sample_images = sorted(glob.glob(f"{config.output_dir}/samples/*.png"))
-# plt.show()
+sample_images = sorted(glob.glob(f"{config.output_dir}/samples/*.png"))
+plt.show()
