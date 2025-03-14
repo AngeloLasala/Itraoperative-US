bf225115d5d584c2c967b95a0ecab080beee22bc
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-03-14 17:20:32 +0000

    updating the codi for traing stable diffusion with visual clip embedding

--- intraoperative_us/diffusion/utils/sbatch_file/train_cond_ldm/stable_diffusion_hf.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/train_cond_ldm/stable_diffusion_hf.sh
@@ -5,13 +5,13 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=18:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=sd_uncond_1.err       # standard error file
-#SBATCH --output=sd_uncond_1.out      # standard output file
+#SBATCH --error=sd_uncond_2.err       # standard error file
+#SBATCH --output=sd_uncond_2.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
 python -m intraoperative_us.diffusion.tools.train_stable_diffusion --conf conf_stable_diffusion\
           --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/ius_diffusion'\
           --trial trial_SD_random\
           --type_image ius\
-          --experiment SD_init_random_cond_image\
+          --experiment SD_init_sd1.5_cond_empty_text\
           --log warning\
