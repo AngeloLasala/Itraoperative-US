3d66b4b0a8cf2f80f2483d875a91ba7bc8244d17
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-03-14 15:36:03 +0000

    initial import of sbatch file for sd training

---
+++ intraoperative_us/diffusion/utils/sbatch_file/train_cond_ldm/stable_diffusion_hf.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=18:00:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=sd_uncond.err       # standard error file
+#SBATCH --output=sd_uncond.out      # standard output file
+#SBATCH --account=IscrC_Med-LMGM     # account name
+
+python -m intraoperative_us.diffusion.tools.train_stable_diffusion --conf conf_stable_diffusion\
+          --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/ius_diffusion'\
+          --trial trial_SD_random\
+          --type_image ius\
+          --experiment SD_init_random_cond_empty_test\
+          --log warning\
