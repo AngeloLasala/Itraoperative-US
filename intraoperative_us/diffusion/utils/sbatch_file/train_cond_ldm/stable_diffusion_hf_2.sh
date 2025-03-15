095d7ecd8e5affd0b3e2499dff220d515b3f0702
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-03-15 09:54:23 +0000

    initial import stable diffusion sbatch file

---
+++ intraoperative_us/diffusion/utils/sbatch_file/train_cond_ldm/stable_diffusion_hf_2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=18:00:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=sd_cond_2.err       # standard error file
+#SBATCH --output=sd_cond_2.out      # standard output file
+#SBATCH --account=IscrC_Med-LMGM     # account name
+
+python -m intraoperative_us.diffusion.tools.train_stable_diffusion --conf conf_stable_diffusion_2\
+          --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/ius_diffusion'\
+          --trial trial_SD_finetuning\
+          --type_image ius\
+          --experiment SD_init_sd1.5_cond_image\
+          --log warning\
