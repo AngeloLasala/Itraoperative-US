535b4e2c9dfd18ae5ac1f1262f739c50a53422da
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-03-16 17:20:54 +0000

    sbatch sample sd cond image

--- intraoperative_us/diffusion/utils/sbatch_file/sample_cond_ldm/sample_stable_diffusion.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/sample_cond_ldm/sample_stable_diffusion.sh
@@ -5,8 +5,8 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=24:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=sample_sd.err       # standard error file
-#SBATCH --output=sample_sd.out      # standard output file
+#SBATCH --error=sample_sd_random.err       # standard error file
+#SBATCH --output=sample_sd_random.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
 for trial in trial_SD_random; do
