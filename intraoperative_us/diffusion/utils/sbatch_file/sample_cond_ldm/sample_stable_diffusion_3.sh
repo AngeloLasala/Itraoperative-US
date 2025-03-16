41a91650dc46aa20c8924b6c0ebc2165b5bd6b40
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-03-16 19:13:12 +0000

    updating the 4 sbatch file for stable diffusion sampling

---
+++ intraoperative_us/diffusion/utils/sbatch_file/sample_cond_ldm/sample_stable_diffusion_3.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=24:00:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=sample_sd_fine_3.err       # standard error file
+#SBATCH --output=sample_sd_fine_3.out      # standard output file
+#SBATCH --account=IscrC_Med-LMGM     # account name
+
+for trial in trial_SD_finetuning; do
+    for w in 1.0 3.0 5.0 7.0 9.0; do
+        for epoch in 1800 2400 3000 ; do
+                python -m intraoperative_us.diffusion.tools.sample_stable_diffusion\
+                        --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/ius_diffusion'\
+                        --generated_mask_dir "/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/ius_diffusion/mask/small_vae/uncond_ldm_1/w_-1.0/ddpm/samples_ep_3000"\
+                        --trial $trial\
+                        --experiment SD_init_sd1.5_cond_image\
+                        --epoch $epoch\
+                        --guide_w $w\
+
+         done
+    done
+done
--- intraoperative_us/diffusion/utils/sbatch_file/sample_cond_ldm/sample_stable_diffusion_4.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/sample_cond_ldm/sample_stable_diffusion_4.sh
@@ -5,18 +5,18 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=24:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=sample_sd_random.err       # standard error file
-#SBATCH --output=sample_sd_random.out      # standard output file
+#SBATCH --error=sample_sd_fine_4.err       # standard error file
+#SBATCH --output=sample_sd_fine_4.out      # standard output file
 #SBATCH --account=IscrC_Med-LMGM     # account name
 
-for trial in trial_SD_random; do
-    for w in 1.0 3.0 5.0 7.0 9.0; do
+for trial in trial_SD_finetuning; do
+    for w in -1.0; do
         for epoch in 1800 2400 3000 ; do
                 python -m intraoperative_us.diffusion.tools.sample_stable_diffusion\
                         --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/ius_diffusion'\
                         --generated_mask_dir "/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/ius_diffusion/mask/small_vae/uncond_ldm_1/w_-1.0/ddpm/samples_ep_3000"\
                         --trial $trial\
-                        --experiment SD_init_random_cond_image\
+                        --experiment SD_init_sd1.5_cond_empty_text\
                         --epoch $epoch\
                         --guide_w $w\
 
