5fd0087f7de9188c4afbcf1393291c54c64039cd
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-03-22 11:17:18 +0000

    updating sbatch file for training SD with 16 bct size

--- intraoperative_us/diffusion/utils/sbatch_file/train_cond_ldm/stable_diffusion_hf_3.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/train_cond_ldm/stable_diffusion_hf_3.sh
@@ -7,11 +7,11 @@
 #SBATCH --partition=boost_usr_prod   # partition name
 #SBATCH --error=sd_cond_3.err       # standard error file
 #SBATCH --output=sd_cond_3.out      # standard output file
-#SBATCH --account=IscrC_Med-LMGM     # account name
+#SBATCH --account=IscrC_AIM-ORAL     # account name
 
 python -m intraoperative_us.diffusion.tools.train_stable_diffusion --conf conf_stable_diffusion_3\
-          --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/ius_diffusion'\
+          --save_folder '/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion'\
           --trial trial_SD_finetuning\
           --type_image ius\
-          --experiment SD_init_random_cond_empty_text\
+          --experiment SD_init_random_cond_empty_text_16\
           --log warning\
