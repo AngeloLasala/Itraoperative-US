9d9ad4e35b18c4d337ff710c5d96bd41c9d2c7db
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-03-22 11:47:10 +0000

    upding compitational time

--- intraoperative_us/diffusion/utils/sbatch_file/train_cond_ldm/stable_diffusion_hf_4.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/train_cond_ldm/stable_diffusion_hf_4.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
-#SBATCH --time=18:00:00                 # time limits: 1 hour
+#SBATCH --time=21:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
 #SBATCH --error=sd_cond_4.err       # standard error file
 #SBATCH --output=sd_cond_4.out      # standard output file
