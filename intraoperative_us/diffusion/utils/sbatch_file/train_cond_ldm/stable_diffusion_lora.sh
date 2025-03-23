eb020897a097950abbc07f4de3d2653d9a203806
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-03-23 09:15:59 +0000

    initial import lora sbatch file

---
+++ intraoperative_us/diffusion/utils/sbatch_file/train_cond_ldm/stable_diffusion_lora.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=18:00:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=lora_1.err       # standard error file
+#SBATCH --output=lora_1.out      # standard output file
+#SBATCH --account=IscrC_AIM-ORAL     # account name
+
+python -m intraoperative_us.diffusion.tools.train_stable_diffusion --conf conf_stable_diffusion_lora_1\
+          --save_folder '/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion'\
+          --trial trial_SD_finetuning\
+          --type_image ius\
+          --experiment SD_init_lora_cond_empty_text\
+          --log warning\
