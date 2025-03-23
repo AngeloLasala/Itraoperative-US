a078a2a8aaea9c0edcd2d708fd829bab68330667
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-03-23 09:49:04 +0000

    updatine cond_ldm_hf sbatch file

--- intraoperative_us/diffusion/utils/sbatch_file/sample_cond_ldm/sample_stable_diffusion_2.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/sample_cond_ldm/sample_stable_diffusion_2.sh
@@ -7,16 +7,16 @@
 #SBATCH --partition=boost_usr_prod   # partition name
 #SBATCH --error=sample_sd_fine_2.err       # standard error file
 #SBATCH --output=sample_sd_fine_2.out      # standard output file
-#SBATCH --account=IscrC_Med-LMGM     # account name
+#SBATCH --account=IscrC_AIM-ORAL     # account name
 
 for trial in trial_SD_finetuning; do
-    for w in 1.0 3.0 5.0 7.0 9.0; do
-        for epoch in 1800 2400 3000 ; do
+    for w in 3.0 5.0 7.0; do
+        for epoch in 3000 6000 9000 ; do
                 python -m intraoperative_us.diffusion.tools.sample_stable_diffusion\
-                        --save_folder '/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/ius_diffusion'\
-                        --generated_mask_dir "/leonardo_work/IscrC_Med-LMGM/Angelo/trained_model/ius_diffusion/mask/small_vae/uncond_ldm_1/w_-1.0/ddpm/samples_ep_3000"\
+                        --save_folder '/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion'\
+                        --generated_mask_dir "/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion/mask/small_vae/uncond_ldm_1/w_-1.0/ddpm/samples_ep_3000"\
                         --trial $trial\
-                        --experiment SD_init_random_cond_image\
+                        --experiment SD_init_random_cond_image_16\
                         --epoch $epoch\
                         --guide_w $w\
 
