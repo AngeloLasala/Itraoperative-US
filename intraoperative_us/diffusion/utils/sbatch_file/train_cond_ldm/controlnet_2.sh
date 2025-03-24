a0dba83a4bd1f43f69edc7a8b8d42f027444356f
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-03-24 11:35:12 +0000

    upload sbatch file controlnet

---
+++ intraoperative_us/diffusion/utils/sbatch_file/train_cond_ldm/controlnet_2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=18:00:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=controlnet_2.err       # standard error file
+#SBATCH --output=controlnet_2.out      # standard output file
+#SBATCH --account=IscrC_AIM-ORAL     # account name
+
+python -m intraoperative_us.diffusion.tools.train_controlnet --conf conf_controlnet_2\
+          --save_folder '/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion'\
+          --trial trial_SD_finetuning\
+          --type_image ius\
+          --experiment SD_init_sd1.5_cond_empty_text_lora\
+          --log warning\
