d4ffe9086c00594528bde0c5403f3f32c7b4ad65
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-03-24 10:53:26 +0000

    check the correct version of tools file for lora

--- intraoperative_us/diffusion/utils/sbatch_file/train_cond_ldm/stable_diffusion_lora.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/train_cond_ldm/stable_diffusion_lora.sh
@@ -9,7 +9,7 @@
 #SBATCH --output=lora_1.out      # standard output file
 #SBATCH --account=IscrC_AIM-ORAL     # account name
 
-python -m intraoperative_us.diffusion.tools.train_stable_diffusion --conf conf_stable_diffusion_lora_1\
+python -m intraoperative_us.diffusion.tools.train_stable_diffusion_lora --conf conf_stable_diffusion_lora_1\
           --save_folder '/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion'\
           --trial trial_SD_finetuning\
           --type_image ius\
