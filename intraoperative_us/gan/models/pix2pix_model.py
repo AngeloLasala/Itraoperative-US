d4830426b21dc6e908ce4a4f5bf90e5df49ca81b
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-03-26 17:57:57 +0000

    updating train

--- intraoperative_us/gan/models/pix2pix_model.py
+++ intraoperative_us/gan/models/pix2pix_model.py
@@ -74,14 +74,25 @@ class Pix2PixModel(BaseModel):
         """Unpack input data from the dataloader and perform necessary pre-processing steps.
 
         Parameters:
-            input (dict): include the data itself and its metadata information.
+            input (list): include the data itself. Coming from the diffusion dataset, it is a list of dictionaries.
+                          input[0]: tensor, ius ultrasound image
+                          input[1]: dict, tumor maask is input[1]['image']
+
 
         The option 'direction' can be used to swap images in domain A and domain B.
         """
         AtoB = self.opt.direction == 'AtoB'
-        self.real_A = input['A' if AtoB else 'B'].to(self.device)
-        self.real_B = input['B' if AtoB else 'A'].to(self.device)
-        self.image_paths = input['A_paths' if AtoB else 'B_paths']
+        ## A - us image, B - tumor mask
+        if self.opt.direction == 'AtoB':
+            self.real_A = input[1]['image'].to(self.device)
+            self.real_B = input[0].to(self.device)
+        elif self.opt.direction == 'BtoA':
+            self.real_A = input[0].to(self.device)
+            self.real_B = input[1]['image'].to(self.device) 
+        else:
+            raise ValueError(f"Direction {self.opt.direction} not recognized")
+        
+        # self.image_paths = input['A_paths' if AtoB else 'B_paths']
 
     def forward(self):
         """Run forward pass; called by both functions <optimize_parameters> and <test>."""
--- intraoperative_us/gan/tools/train.py
+++ intraoperative_us/gan/tools/train.py
@@ -21,12 +21,43 @@ See frequently asked questions at: https://github.com/junyanz/pytorch-CycleGAN-a
 """
 import time
 import logging
-
+import os
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import json
+import torch
 from torch.utils.data import DataLoader
+import numpy as np
 
 from intraoperative_us.gan.options.train_options import TrainOptions
 from intraoperative_us.diffusion.dataset.dataset import IntraoperativeUS
 from intraoperative_us.gan.models import create_model
+from intraoperative_us.gan.utils.visualizer import Visualizer
+
+def visualize_results(model, epoch, save_folder):
+    """
+    Visualize the results of the model
+    """
+    img = model.real_B[0].cpu().detach()
+    img = torch.clamp(img, -1., 1.).numpy().transpose(1, 2, 0)
+    img = (img + 1) / 2
+
+    gen_img = model.fake_B[0].cpu().detach()
+    gen_img = torch.clamp(gen_img, -1., 1.).numpy().transpose(1, 2, 0)
+    gen_img = (gen_img + 1) / 2
+
+    fig, ax = plt.subplots(1, 3, figsize=(15, 5), num=f'Results_epch_{epoch}', tight_layout=True)
+    ax[0].set_title('Tumor mask ', fontsize=28)
+    ax[0].imshow(model.real_A[0].cpu().detach().numpy().transpose(1, 2, 0), cmap='gray')
+    ax[0].axis('off')
+    ax[1].set_title('Real Image', fontsize=28)
+    ax[1].imshow(img, cmap='gray')
+    ax[1].axis('off')
+    ax[2].set_title('Generated Image', fontsize=28)
+    ax[2].imshow(gen_img, cmap='gray')
+    ax[2].axis('off')
+
+    plt.savefig(os.path.join(opt.checkpoints_dir, opt.name, 'web', f'epoch_{epoch}.png'))
 
 if __name__ == '__main__':
     ## Training Configuration
@@ -50,47 +81,70 @@ if __name__ == '__main__':
                                data_augmentation=True
                                )
 
-    data_loader = DataLoader(data_img, batch_size=opt.batch_size, shuffle=True, num_workers=8)
+    dataset = DataLoader(data_img, batch_size=opt.batch_size, shuffle=True, num_workers=8)
     dataset_size = len(data_img)
     logging.info(f'len of the dataset: {dataset_size}')
+
+    ## Create visualizer
+    visualizer = Visualizer(opt)   # create a visualizer that display/save images and plots 
 
     ## Create model
     model = create_model(opt)      # create a model given opt.model and other options
     model.setup(opt)               # regular setup: load and print networks; create schedulers
     total_iters = 0                # the total number of training iterations
 
+    save_folder = os.path.join(opt.checkpoints_dir, opt.name)
+    if not os.path.exists(save_folder):
+        os.makedirs(save_folder)
+    save_folder_images = os.path.join(save_folder, 'web')
+    if not os.path.exists(save_folder_images):
+        os.makedirs(save_folder_images)
+    save_folder_models = os.path.join(save_folder, 'models')
+    if not os.path.exists(save_folder_models):
+        os.makedirs(save_folder_models)
+    
+    losses_dict = {'G_GAN': [], 'G_L1': [], 'D_real': [], 'D_fake': []}
     for epoch in range(opt.epoch_count, opt.n_epochs + opt.n_epochs_decay + 1):    # outer loop for different epochs; we save the model by <epoch_count>, <epoch_count>+<save_latest_freq>
         epoch_start_time = time.time()  # timer for entire epoch
         iter_data_time = time.time()    # timer for data loading per iteration
         epoch_iter = 0                  # the number of training iterations in current epoch, reset to 0 every epoch
         model.update_learning_rate()    # update learning rates in the beginning of every epoch.
+        #self.optimizers[0].param_groups[0]['lr']
+
+        progress_bar = tqdm(total=dataset_size, disable=False)
+        progress_bar.set_description(f"Epoch {epoch}/{opt.n_epochs + opt.n_epochs_decay}")
+        g_gan, g_l1, d_real, d_fake = [], [], [], [] 
         for i, data in enumerate(dataset):  # inner loop within one epoch
             iter_start_time = time.time()  # timer for computation per iteration
-            print(data)
             if total_iters % opt.print_freq == 0:
                 t_data = iter_start_time - iter_data_time
 
-    #         total_iters += opt.batch_size
-    #         epoch_iter += opt.batch_size
-    #         model.set_input(data)         # unpack data from dataset and apply preprocessing
-    #         model.optimize_parameters()   # calculate loss functions, get gradients, update network weights
+            total_iters += opt.batch_size
+            epoch_iter += opt.batch_size
+            model.set_input(data)         # unpack data from dataset and apply preprocessing
+            model.optimize_parameters()   # calculate loss functions, get gradients, update network weights
 
-    #         # print training losses and save logging information to the disk
-    #         losses = model.get_current_losses()
-    #         t_comp = (time.time() - iter_start_time) / opt.batch_size
-    #         # visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data)
-    #         # if opt.display_id > 0:
-    #         #     visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
+            if total_iters % (opt.display_freq * dataset_size) == 0:   # display images on visdom and save images to a HTML file
+                visualize_results(model, epoch, save_folder_images)
+             
+            progress_bar.update(1)
+            losses = model.get_current_losses()
+            g_gan.append(losses['G_GAN'])
+            g_l1.append(losses['G_L1'])
+            d_real.append(losses['D_real'])
+            d_fake.append(losses['D_fake'])
+            logs = {"G_GAN": losses['G_GAN'], "D_real": losses['D_real'], "D_fake": losses['D_fake'], "lr": model.optimizers[0].param_groups[0]['lr']}
+            progress_bar.set_postfix(**logs)
 
-    #         if total_iters % opt.save_latest_freq == 0:   # cache our latest model every <save_latest_freq> iterations
-    #             print('saving the latest model (epoch %d, total_iters %d)' % (epoch, total_iters))
-    #             save_suffix = 'iter_%d' % total_iters if opt.save_by_iter else 'latest'
-    #             model.save_networks(save_suffix)
+        losses_dict['G_GAN'].append(np.mean(g_gan))
+        losses_dict['G_L1'].append(np.mean(g_l1))
+        losses_dict['D_real'].append(np.mean(d_real))
+        losses_dict['D_fake'].append(np.mean(d_fake))
+        
+        if epoch % opt.save_epoch_freq == 0:              # cache our model every <save_epoch_freq> epochs
+            torch.save(model.netG.state_dict(), os.path.join(save_folder_models, f'netG_{epoch}.pth'))
+            torch.save(model.netD.state_dict(), os.path.join(save_folder_models, f'netD_{epoch}.pth'))
 
-    #         iter_data_time = time.time()
-    #     if epoch % opt.save_epoch_freq == 0:              # cache our model every <save_epoch_freq> epochs
-    #         print('saving the model at the end of epoch %d, iters %d' % (epoch, total_iters))
-    #         model.save_networks('latest')
-    #         model.save_networks(epoch)
-
-    #     print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.n_epochs + opt.n_epochs_decay, time.time() - epoch_start_time))
+    with open(os.path.join(save_folder, 'losses.json'), 'w') as f:
+        json.dump(losses_dict, f, indent=4)
+    logging.info(f"Training finished")
