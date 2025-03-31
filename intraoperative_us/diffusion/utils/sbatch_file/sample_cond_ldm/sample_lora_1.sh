f44eb2a7c87412d32d90a92abf34ad69ab105b66
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-03-31 09:50:56 +0100

    import sample sbatch file for lora finetuning model

---
+++ intraoperative_us/diffusion/utils/sbatch_file/sample_cond_ldm/sample_lora_1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=24:00:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=lora_fine_1.err       # standard error file
+#SBATCH --output=lora_fine_1.out      # standard output file
+#SBATCH --account=IscrC_AIM-ORAL     # account name 
+
+for trial in trial_SD_finetuning; do
+    for w in -1.0; do
+        for epoch in 1200 1800 2400 3000 ; do
+                python -m intraoperative_us.diffusion.tools.sample_stable_diffusion\
+                        --save_folder '/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion'\
+                        --generated_mask_dir "/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion/mask/small_vae/uncond_ldm_1/w_-1.0/ddpm/samples_ep_3000"\
+                        --trial $trial\
+                        --experiment SD_init_lora_cond_empty_text\
+                        --epoch $epoch\
+                        --guide_w $w\
+
+         done
+    done
+done
