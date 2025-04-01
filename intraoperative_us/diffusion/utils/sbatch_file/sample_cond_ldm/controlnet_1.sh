c3bc59d84a365bb86f0a467a01a1fc0fccc1d99b
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-04-01 16:01:00 +0100

    import controlnet sample

--- intraoperative_us/diffusion/utils/sbatch_file/sample_cond_ldm/controlnet_1.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/sample_cond_ldm/controlnet_1.sh
@@ -12,7 +12,7 @@
 for trial in trial_SD_finetuning; do
     for w in 7.0; do
         for epoch in 1200 1800 2400 3000 ; do
-                python -m intraoperative_us.diffusion.tools.sample_stable_diffusion\
+                python -m intraoperative_us.diffusion.tools.sample_controlnet\
                         --save_folder '/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion'\
                         --generated_mask_dir "/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion/mask/small_vae/uncond_ldm_1/w_-1.0/ddpm/samples_ep_3000"\
                         --trial $trial\
