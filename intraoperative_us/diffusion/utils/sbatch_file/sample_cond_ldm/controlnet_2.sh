e77be3c1bfd1e397d4585941d21e9d0a44582257
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-04-06 09:49:14 +0100

    update controlnet sample sbatch

---
+++ intraoperative_us/diffusion/utils/sbatch_file/sample_cond_ldm/controlnet_2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=24:00:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=controlnet_2.err       # standard error file
+#SBATCH --output=controlnet_2.out      # standard output file
+#SBATCH --account=IscrC_AIM-ORAL     # account name 
+
+for trial in trial_SD_finetuning trial_SD_random; do
+    for w in 3.0 5.0 7.0; do
+        for epoch in 5000 7000 8000 9000 ; do
+                python -m intraoperative_us.diffusion.tools.sample_controlnet\
+                        --save_folder '/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion'\
+                        --generated_mask_dir "/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion/mask/small_vae/uncond_ldm_1/w_-1.0/ddpm/samples_ep_3000"\
+                        --trial $trial\
+                        --experiment Controlnet_lora_empty_text\
+                        --epoch $epoch\
+                        --guide_w $w\
+                        --log info\
+
+         done
+    done
+done
