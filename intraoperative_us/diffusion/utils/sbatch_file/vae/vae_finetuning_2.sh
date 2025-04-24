5fe0bf2430f3440e87e1fd8f2e6776cfe436849f
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-04-24 18:51:29 +0100

    a

--- intraoperative_us/diffusion/utils/sbatch_file/vae/vae_finetuning_2.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/vae/vae_finetuning_2.sh
@@ -5,11 +5,11 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=9:50:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=vae_fine_0.err       # standard error file
-#SBATCH --output=vae_fine_0.out      # standard output file
+#SBATCH --error=vae_fine_2.err       # standard error file
+#SBATCH --output=vae_fine_2.out      # standard output file
 #SBATCH --account=IscrC_AIM-ORAL     # account name
 
-python -m intraoperative_us.diffusion.tools.train_vae --conf cond_ldm/conf_vae_finetuning_0\
+python -m intraoperative_us.diffusion.tools.train_vae --conf cond_ldm/conf_vae_finetuning_2\
                                                       --save_folder '/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion'\
-                                                      --trial_name VAE_finetuning/split_0\
+                                                      --trial_name VAE_finetuning/split_2\
                                                       --log 'info'
