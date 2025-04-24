8bd124918d42f2faf84940f5f94c51df122d0adf
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-04-24 16:22:47 +0100

    update train_vae

---
+++ intraoperative_us/diffusion/utils/sbatch_file/vae/vae_random_0.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=9:50:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=vae_fine_hug.err       # standard error file
+#SBATCH --output=vae_fine_hug.out      # standard output file
+#SBATCH --account=IscrC_AIM-ORAL     # account name
+
+python -m intraoperative_us.diffusion.tools.train_vae --conf cond_ldm/conf_vae_random_0\
+                                                      --save_folder '/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion'\
+                                                      --trial_name VAE_random/splti_0\
+                                                      --log 'info'
