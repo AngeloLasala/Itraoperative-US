27d614bdfe7b86fc939b8d57b94ce72ed4d4ba8b
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-04-27 10:44:13 +0100

    update conf and sbatch file for SD finetuning

--- intraoperative_us/diffusion/utils/sbatch_file/train_cond_ldm/stable_diffusion_lora_2.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/train_cond_ldm/stable_diffusion_lora_2.sh
@@ -5,13 +5,13 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=24:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=finetuning_3.err       # standard error file
-#SBATCH --output=finetuning_3.out      # standard output file
+#SBATCH --error=lora_2.err       # standard error file
+#SBATCH --output=lora_2.out      # standard output file
 #SBATCH --account=IscrC_AIM-ORAL     # account name
 
-python -m intraoperative_us.diffusion.tools.train_stable_diffusion_lora --conf conf_stable_diffusion_lora_1\
+python -m intraoperative_us.diffusion.tools.train_stable_diffusion_lora --conf sd1.5/conf_stable_diffusion_lora_1\
           --save_folder '/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion'\
-          --trial trial_SD_finetuning\
+          --trial VAE_random\
           --type_image ius\
           --experiment SD_lora_empty_text\
           --log warning\
