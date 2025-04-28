ee99df9df6828fea20e7993cbf7dd18f65c4edfe
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-04-28 10:57:20 +0100

    upload initial import of sbatch file for one_step generation

---
+++ intraoperative_us/diffusion/utils/sbatch_file/one_step/vae.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=9:50:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=vae_fine_1.err       # standard error file
+#SBATCH --output=vae_fine_1.out      # standard output file
+#SBATCH --account=IscrC_AIM-ORAL     # account name
+
+for split in 0 1 ; do
+    python -m intraoperative_us.diffusion.tools.train_vae_one_step --conf one_step/conf_one_step_$split\
+                                                        --save_folder '/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion'\
+                                                        --log 'info'
+done
