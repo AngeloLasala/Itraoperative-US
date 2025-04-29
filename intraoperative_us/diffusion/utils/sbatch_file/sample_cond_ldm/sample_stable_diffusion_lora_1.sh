41f14e23efcca863365e82926b4a7e6a713e4b62
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-04-29 16:17:20 +0100

    upload sbatch file for lora sampling

---
+++ intraoperative_us/diffusion/utils/sbatch_file/sample_cond_ldm/sample_stable_diffusion_lora_1.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --nodes=1                    # 1 node
+#SBATCH --ntasks=1
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-node=1          # 1 tasks per node
+#SBATCH --time=3:00:00                 # time limits: 1 hour
+#SBATCH --partition=boost_usr_prod   # partition name
+#SBATCH --error=sd_random_1.err       # standard error file
+#SBATCH --output=sd_finetuning_1.out      # standard output file
+#SBATCH --account=IscrC_AIM-ORAL     # account name 
+
+for trial in VAE_finetuning VAE_random; do
+    for w in -1.0; do
+        for epoch in 5000 6000 7000 8000 9000 ; do
+                python -m intraoperative_us.diffusion.tools.sample_stable_diffusion\
+                        --save_folder '/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion'\
+                        --generated_mask_dir "/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion/mask/split_1/uncond_ldm/w_-1.0/ddpm/samples_ep_3000"\
+                        --trial $trial\
+                        --split 'split_1'\
+                        --experiment SD_lora_empty_text\
+                        --epoch $epoch\
+                        --guide_w $w\
+                        --scheduler 'dpm_solver'\
+                        --num_sample_timesteps 30\
+
+         done
+    done
+done
