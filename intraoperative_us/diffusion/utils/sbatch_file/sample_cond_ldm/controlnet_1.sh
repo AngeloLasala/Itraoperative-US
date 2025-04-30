51720f3589569dad241ce947fd2abf88fbca55b9
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-04-30 09:23:31 +0100

    upload sbatch file for sampling controlnet

--- intraoperative_us/diffusion/utils/sbatch_file/sample_cond_ldm/controlnet_1.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/sample_cond_ldm/controlnet_1.sh
@@ -5,21 +5,23 @@
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
 #SBATCH --time=24:00:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
-#SBATCH --error=controlnet_1.err       # standard error file
-#SBATCH --output=controlnet_1.out      # standard output file
+#SBATCH --error=controlnet_lora.err       # standard error file
+#SBATCH --output=controlnet_lora.out      # standard output file
 #SBATCH --account=IscrC_AIM-ORAL     # account name 
 
-for trial in trial_SD_finetuning trial_SD_random; do
-    for w in 3.0 5.0 7.0; do
-        for epoch in 5000 7000 8000 9000 ; do
+for trial in VAE_finetuning VAE_random; do
+    for w in 3.0; do
+        for epoch in 5000 6000 7000 8000 9000 ; do
                 python -m intraoperative_us.diffusion.tools.sample_controlnet\
                         --save_folder '/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion'\
-                        --generated_mask_dir "/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion/mask/small_vae/uncond_ldm_1/w_-1.0/ddpm/samples_ep_3000"\
+                        --generated_mask_dir "/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion/mask/split_1/uncond_ldm/w_-1.0/ddpm/samples_ep_3000"\
                         --trial $trial\
-                        --experiment Controlnet_sd1.5_empty_text\
+                        --split 'split_1'\
+                        --experiment Controlnet_lora_empty_text\
                         --epoch $epoch\
                         --guide_w $w\
-                        --log info\
+                        --scheduler 'dpm_solver'\
+                        --num_sample_timesteps 30\
 
          done
     done
--- intraoperative_us/diffusion/utils/sbatch_file/sample_cond_ldm/controlnet_2.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/sample_cond_ldm/controlnet_2.sh
@@ -9,17 +9,19 @@
 #SBATCH --output=controlnet_2.out      # standard output file
 #SBATCH --account=IscrC_AIM-ORAL     # account name 
 
-for trial in trial_SD_finetuning trial_SD_random; do
-    for w in 3.0 5.0 7.0; do
-        for epoch in 5000 7000 8000 9000 ; do
+for trial in VAE_finetuning VAE_random; do
+    for w in 3.0; do
+        for epoch in 5000 6000 7000 8000 9000 ; do
                 python -m intraoperative_us.diffusion.tools.sample_controlnet\
                         --save_folder '/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion'\
-                        --generated_mask_dir "/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion/mask/small_vae/uncond_ldm_1/w_-1.0/ddpm/samples_ep_3000"\
+                        --generated_mask_dir "/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion/mask/split_1/uncond_ldm/w_-1.0/ddpm/samples_ep_3000"\
                         --trial $trial\
-                        --experiment Controlnet_lora_empty_text\
+                        --split 'split_1'\
+                        --experiment Controlnet_finetuning_empty_text\
                         --epoch $epoch\
                         --guide_w $w\
-                        --log info\
+                        --scheduler 'dpm_solver'\
+                        --num_sample_timesteps 30\
 
          done
     done
