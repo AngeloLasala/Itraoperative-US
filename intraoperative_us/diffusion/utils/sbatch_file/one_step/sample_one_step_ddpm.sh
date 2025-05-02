7418cce2e15de35c6e230066e22e41d766cbac83
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-05-02 10:31:33 +0100

    sampling one step

--- intraoperative_us/diffusion/utils/sbatch_file/one_step/sample_one_step_ddpm.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/one_step/sample_one_step_ddpm.sh
@@ -12,7 +12,7 @@
 for exp in ldm_finetuning; do
     for w in -1.0; do
         for epoch in 5000 6000 7000 8000 9000 ; do
-                python -m intraoperative_us.diffusion.tools.sample_cond_ldm_hugginface\
+                python -m intraoperative_us.diffusion.tools.sample_one_step\
                         --save_folder '/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion'\
                         --trial trial_1\
                         --split 'split_1'\
