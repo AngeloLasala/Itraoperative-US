d19f27ea578904d77111b206aa018449165d0272
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-05-18 09:49:58 +0100

    update the simple version of one step generation

--- intraoperative_us/diffusion/utils/sbatch_file/one_step/vae.sh
+++ intraoperative_us/diffusion/utils/sbatch_file/one_step/vae.sh
@@ -3,15 +3,15 @@
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks-per-node=1          # 1 tasks per node
-#SBATCH --time=9:50:00                 # time limits: 1 hour
+#SBATCH --time=23:50:00                 # time limits: 1 hour
 #SBATCH --partition=boost_usr_prod   # partition name
 #SBATCH --error=vae_stack.err       # standard error file
 #SBATCH --output=vae_stack.out      # standard output file
 #SBATCH --account=IscrC_AIM-ORAL     # account name
 
-for split in 1 ; do
-    python -m intraoperative_us.diffusion.tools.train_vae_one_step --conf one_step/conf_one_step_1\
+for split in 1 0 2 ; do
+    python -m intraoperative_us.diffusion.tools.train_vae_one_step --conf one_step/conf_one_step_$split\
                                                         --save_folder '/leonardo_work/IscrC_AIM-ORAL/Angelo/trained_model/ius_diffusion'\
-                                                        --trial Stack_finetuning/split_1\
+                                                        --trial Stack_finetuning/split_$split\
                                                         --log 'info'
 done
