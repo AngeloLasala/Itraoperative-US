ffcfbae72836b709a31fde24b05341bcf4298e0d
AngeloLasala <Angelo.Lasala@santannapisa.it>
2025-06-09 09:05:16 +0100

    update everything

--- intraoperative_us/segmentation/tools/nnUnet.sh
+++ intraoperative_us/segmentation/tools/nnUnet.sh
@@ -6,36 +6,36 @@ export nnUNet_results="/media/angelo/OS/Users/lasal/OneDrive - Scuola Superiore 
 
 # create dataset Real Real_Gex_x#numeber
 # python -m intraoperative_us.segmentation.dataset.create_nnUnet_dataset --save_folder "/media/angelo/OS/Users/lasal/OneDrive - Scuola Superiore Sant'Anna/PhD_notes/Visiting_Imperial/trained_model/"\
-#                                                                        --name_dataset "RealGenx2" \
+#                                                                        --name_dataset "RealGenx3" \
 #                                                                        --trial 'VAE_finetuning' \
 #                                                                        --experiment cond_ldm_finetuning
 
 # # analyse raw data
-# nnUNetv2_plan_and_preprocess -d 2 --verify_dataset_integrity
+# nnUNetv2_plan_and_preprocess -d 4 --verify_dataset_integrity
 
 # create split
-python -m intraoperative_us.segmentation.dataset.create_nnUnet_split -d 2
+# python -m intraoperative_us.segmentation.dataset.create_nnUnet_split -d 4
 
 # # train the model
-# for split in 0 1 2 3 4 ; do
-#     nnUNetv2_train 1 2d $split -device cuda
+# for split in 4 ; do
+#     nnUNetv2_train 4 2d $split -device cuda
 # done
 
 # best configuration
-# nnUNetv2_find_best_configuration 1
+# nnUNetv2_find_best_configuration 4
 
 # predict
-# nnUNetv2_predict -d 1\
+# nnUNetv2_predict -d 4\
 #                  -i "/media/angelo/OS/Users/lasal/OneDrive - Scuola Superiore Sant'Anna/PhD_notes/Visiting_Imperial/RESECT_iUS_dataset/nnUNet_raw/Dataset001_Real/imagesTs" \
-#                  -o "/media/angelo/OS/Users/lasal/OneDrive - Scuola Superiore Sant'Anna/PhD_notes/Visiting_Imperial/trained_model/nnUNet_results/Dataset001_Real/inference" \
+#                  -o "/media/angelo/OS/Users/lasal/OneDrive - Scuola Superiore Sant'Anna/PhD_notes/Visiting_Imperial/trained_model/nnUNet_results/Dataset004_RealGenx3/inference" \
 #                  -f  0 1 2 3 4 \
 #                  -tr nnUNetTrainer \
 #                  -c 2d \
 #                  -p nnUNetPlans
 
 # postprocess
-# nnUNetv2_apply_postprocessing -i "/media/angelo/OS/Users/lasal/OneDrive - Scuola Superiore Sant'Anna/PhD_notes/Visiting_Imperial/trained_model/nnUNet_results/Dataset001_Real/inference" \
-#                               -o "/media/angelo/OS/Users/lasal/OneDrive - Scuola Superiore Sant'Anna/PhD_notes/Visiting_Imperial/trained_model/nnUNet_results/Dataset001_Real/inference/post_process" \
-#                               -pp_pkl_file "/media/angelo/OS/Users/lasal/OneDrive - Scuola Superiore Sant'Anna/PhD_notes/Visiting_Imperial/trained_model/nnUNet_results/Dataset001_Real/nnUNetTrainer__nnUNetPlans__2d/crossval_results_folds_0_1_2_3_4/postprocessing.pkl" \
-#                                -np 8 \
-#                                -plans_json "/media/angelo/OS/Users/lasal/OneDrive - Scuola Superiore Sant'Anna/PhD_notes/Visiting_Imperial/trained_model/nnUNet_results/Dataset001_Real/nnUNetTrainer__nnUNetPlans__2d/crossval_results_folds_0_1_2_3_4/plans.json"
+nnUNetv2_apply_postprocessing -i "/media/angelo/OS/Users/lasal/OneDrive - Scuola Superiore Sant'Anna/PhD_notes/Visiting_Imperial/trained_model/nnUNet_results/Dataset004_RealGenx3/inference" \
+                              -o "/media/angelo/OS/Users/lasal/OneDrive - Scuola Superiore Sant'Anna/PhD_notes/Visiting_Imperial/trained_model/nnUNet_results/Dataset004_RealGenx3/inference/post_process" \
+                              -pp_pkl_file "/media/angelo/OS/Users/lasal/OneDrive - Scuola Superiore Sant'Anna/PhD_notes/Visiting_Imperial/trained_model/nnUNet_results/Dataset004_RealGenx3/nnUNetTrainer__nnUNetPlans__2d/crossval_results_folds_0_1_2_3_4/postprocessing.pkl" \
+                               -np 8 \
+                               -plans_json "/media/angelo/OS/Users/lasal/OneDrive - Scuola Superiore Sant'Anna/PhD_notes/Visiting_Imperial/trained_model/nnUNet_results/Dataset004_RealGenx3/nnUNetTrainer__nnUNetPlans__2d/crossval_results_folds_0_1_2_3_4/plans.json"
